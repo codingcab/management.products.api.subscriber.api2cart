f39a53d1f953b52fde7b81beb54d56a8a2da8742
CMHayden <haydencallum4@gmail.com>
2019-07-19 12:42:22 +0200

    create route for adding product

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('api2cart', 'SNSController@handleRequest');
+Route::post('addProduct', 'AddProductController@handleRequest');
