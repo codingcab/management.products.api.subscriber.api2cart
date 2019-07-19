9c51568185c092c8213ff9ac046e5e3621f3f0fb
CMHayden <haydencallum4@gmail.com>
2019-07-19 11:28:17 +0200

    add subscribing logic

--- app/Http/Controllers/SNSController.php
+++ app/Http/Controllers/SNSController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Log;
 
 class SNSController extends Controller
 {
-    //
+    public function handleRequest(Request $request) {
+
+        $requestJSON = json_decode($request->getContent(), true);
+
+        if($requestJSON['Type'] == 'SubscriptionConfirmation') {
+            return $this->subscribe($requestJSON);
+        }
+
+    }
+
+    private function subscribe($notification) {
+
+        Log::info('Subscribing to topic');
+
+        $guzzleClient = new \GuzzleHttp\Client();
+
+        $guzzleResponse = $guzzleClient->get($notification['SubscribeURL']);
+
+        Log::info('Successfully subscribed');
+
+        Log::info($guzzleResponse);
+
+        return app('Illuminate\Http\Response')->status();
+
+    }
 }
