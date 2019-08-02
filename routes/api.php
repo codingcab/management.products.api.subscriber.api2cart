6b224ebb2ecb2bba572fcb14cad899b7c233e0c5
CMHayden <haydencallum4@gmail.com>
2019-08-02 19:20:32 +0200

    remove test route

--- routes/api.php
+++ routes/api.php
@@ -19,16 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('addProduct', 'SNSController@handleRequest');
 
-Route::get('test', function () {
 
-
-    $guzzleClient = new \GuzzleHttp\Client();
-
-    $guzzleResponse = $guzzleClient->request('get', 'https://api.api2cart.com/v1.0/product.list.json?api_key=e3d14cb857bb7c271f26c1fbe2b00470&store_key=ed58a22dfecb405a50ea3ea56979360d');
-
-    $testResponse = $guzzleResponse->getBody();
-
-    return $testResponse;
-    Log::info($testResponse);
-
-});
