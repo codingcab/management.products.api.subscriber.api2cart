650809416d3dd0f451ac9fdb077e7aed94df1fc5
CMHayden <haydencallum4@gmail.com>
2019-08-05 13:47:01 +0200

    update route to match new controller name and accepter storekey variable

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('addProduct', 'SNSController@handleRequest');
+Route::post('addProduct/{storekey}', 'ProductsController@handleRequest');
 
 
