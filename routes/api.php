27ed29e10b017715a764ad35e6d753deb6c542a9
Artur Hanusek <arthur@youritsolutions.ie>
2019-09-02 15:54:53 +0200

    change route

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('addProduct/{storekey}', 'ProductsController@handleRequest');
+Route::post('store/{storekey}/products', 'ProductsController@handleRequest');
 
 
