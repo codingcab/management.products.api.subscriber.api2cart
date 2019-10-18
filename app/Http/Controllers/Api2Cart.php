c939d9acd2e22ea5ad1887df5bdcaea3402c0f9f
Artur Hanusek <arthur@youritsolutions.ie>
2019-10-18 09:29:13 +0200

    update sale price key names

--- app/Http/Controllers/Api2Cart.php
+++ app/Http/Controllers/Api2Cart.php
@@ -246,9 +246,9 @@ class Api2Cart extends Controller
         $newElement = $this->array_add_if_exists($data, $newElement, "store_id");
         $newElement = $this->array_add_if_exists($data, $newElement, "price");
         $newElement = $this->array_add_if_exists($data, $newElement, "quantity");
-        $newElement = $this->array_add_if_exists($data, $newElement, "special_price");
-        $newElement = $this->array_add_if_exists($data, $newElement, "sprice_create");
-        $newElement = $this->array_add_if_exists($data, $newElement, "sprice_expire");
+        $newElement = $this->array_add_if_exists($data, $newElement, "sale_price", "special_price");
+        $newElement = $this->array_add_if_exists($data, $newElement, "sale_price_start_date","sprice_create");
+        $newElement = $this->array_add_if_exists($data, $newElement, "sale_price_end_date", "sprice_expire");
         $newElement = $this->array_add_if_exists($data, $newElement, "in_stock");
         $newElement = $this->array_add_if_exists($data, $newElement, "weight");
 
