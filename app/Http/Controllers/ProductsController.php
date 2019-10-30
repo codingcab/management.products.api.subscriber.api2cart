bf1f9866e193fac109924ea94fc903cc201306e3
Artur Hanusek <artur@coding.cab>
2019-10-30 12:18:44 +0000

    generate product data and pass it to api2cart for update

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Log;
 use function Psy\debug;
 
@@ -15,12 +17,37 @@ class ProductsController extends BaseSnsController
 
         $api2cart = new \App\Http\Controllers\Api2Cart($api_key, $store_key);
 
-        $product_data = $notification;
-
-        $product_data["quantity"] = $notification["quantity_available"];
+        $product_data = $this->generateProductData($notification);
 
         if($api2cart->productUpdateOrCreate($product_data)) {
             $this->respond_ok_200();
         }
+    }
+
+    /**
+     * @param $notification
+     * @return mixed
+     */
+    private function generateProductData($notification)
+    {
+        $product = [];
+
+        $product["sku"]             = $notification["sku"];
+        $product["price"]           = $notification["price"];
+        $product["special_price"]   = $notification["sale_price"];
+        $product["sprice_create"]   = $notification["sale_price_start_date"];
+        $product["sprice_expire"]   = $notification["sale_price_end_date"];
+
+        $product["quantity"]        = intval($notification["quantity_available"]);
+
+        if ($product["quantity"] > 0) {
+            $product["in_stock"] = true;
+        }
+
+        if (Arr::has($notification, "store_id")) {
+            $product["store_id"] = $notification["store_id"];
+        }
+
+        return $product;
     }
 }
