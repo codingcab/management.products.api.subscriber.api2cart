bde07354595c880b6a1c236254e69612922352bf
Artur Hanusek <arthur@youritsolutions.ie>
2020-02-04 09:55:05 +0000

    generate constructor in Api2CartResponse class

--- app/Api2cart/Api2CartResponse.php
+++ app/Api2cart/Api2CartResponse.php
@@ -4,7 +4,18 @@
 namespace App\Api2cart;
 
 
+use Psr\Http\Message\ResponseInterface;
+
 class Api2CartResponse
 {
+    private $response;
 
+    /**
+     * Api2CartResponse constructor.
+     * @param ResponseInterface $response
+     */
+    public function __construct(ResponseInterface $response)
+    {
+        $this->response = $response;
+    }
 }
