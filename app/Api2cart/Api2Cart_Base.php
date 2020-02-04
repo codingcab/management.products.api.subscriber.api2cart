de58b6da7eafd0b68d3fc02d80adf4c59ab41c19
Artur Hanusek <arthur@youritsolutions.ie>
2020-02-04 09:08:47 +0000

    move constructor to base class@

--- app/Api2cart/Api2Cart_Base.php
+++ app/Api2cart/Api2Cart_Base.php
@@ -8,5 +8,23 @@ use GuzzleHttp\Client;
 
 class Api2Cart_Base
 {
+    public $guzzle;
+
+    private $exceptions = true;
+    public $store_key = null;
+    public $api_key = null;
+
+    public function __construct(String $store_key, bool $exceptions = true)
+    {
+        $this->exceptions = $exceptions;
+        $this->api_key = env('API2CART_API_KEY', '');
+        $this->store_key = $store_key;
+
+        $this->guzzle = new Client([
+            'base_uri' =>  'https://api.api2cart.com/v1.1/',
+            'timeout' => 60,
+            'exceptions' => true,
+        ]);
+    }
 
 }
--- app/Http/Controllers/Api2cartController.php
+++ app/Http/Controllers/Api2cartController.php
@@ -34,28 +34,9 @@ class Api2cartController extends Api2Cart_Base
 
     public $lastResponse;
 
-    private $guzzle;
-
-    private $exceptions = true;
-    private $store_key = null;
-    private $api_key = null;
-
     public static function connect(String $store_key)
     {
         return new Api2cartController($store_key);
-    }
-
-    public function __construct(String $store_key, bool $exceptions = true)
-    {
-        $this->exceptions = $exceptions;
-        $this->api_key = env('API2CART_API_KEY', '');
-        $this->store_key = $store_key;
-
-        $this->guzzle = new Client([
-            'base_uri' =>  'https://api.api2cart.com/v1.1/',
-            'timeout' => 60,
-            'exceptions' => true,
-        ]);
     }
 
     public function listVariants()
