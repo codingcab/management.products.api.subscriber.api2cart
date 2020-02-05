16f72771b4bec32502b678b091c738bc694adebf
Artur Hanusek <arthur@youritsolutions.ie>
2020-02-05 09:00:25 +0000

    add _client variable and getter for it

--- app/Api2cart/Entity.php
+++ app/Api2cart/Entity.php
@@ -6,8 +6,27 @@ namespace App\API2CART;
 
 class Entity extends Client
 {
+    /**
+     * @var Client
+     */
+    private $_client;
+
+    /**
+     * Entity constructor.
+     * @param string $store_key
+     * @param bool $exceptions
+     */
     public function __construct(string $store_key, bool $exceptions = true)
     {
         parent::__construct($store_key, $exceptions);
+        $this->client = new Client($store_key, $exceptions);
+    }
+
+    /**
+     * @return Client
+     */
+    public function client()
+    {
+        return $this->_client;
     }
 }
