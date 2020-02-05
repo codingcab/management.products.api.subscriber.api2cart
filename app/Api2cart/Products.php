9c7964788676b65100d3c1d0142fbe0db6f8009e
Artur Hanusek <arthur@youritsolutions.ie>
2020-02-05 14:02:35 +0000

    add SKU to allowed keys

--- app/Api2cart/Products.php
+++ app/Api2cart/Products.php
@@ -8,9 +8,9 @@ use Illuminate\Support\Facades\Log;
 
 class Products extends Entity
 {
-
     private const PRODUCT_ALLOWED_KEYS = [
         "id",
+        "sku",
         "model",
         "name",
         "description",
