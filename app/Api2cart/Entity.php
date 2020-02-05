88fa574dadc71b59d186c132283affd6ac59eeb7
Artur Hanusek <arthur@youritsolutions.ie>
2020-02-05 08:56:50 +0000

    add constructor to Entity class

--- app/Api2cart/Entity.php
+++ app/Api2cart/Entity.php
@@ -6,5 +6,8 @@ namespace App\API2CART;
 
 class Entity extends Client
 {
-
+    public function __construct(string $store_key, bool $exceptions = true)
+    {
+        parent::__construct($store_key, $exceptions);
+    }
 }
