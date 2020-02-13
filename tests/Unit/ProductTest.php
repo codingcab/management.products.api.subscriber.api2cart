82df1ea07e247d8828b144e53d3457d9c92a5e64
Artur Hanusek <arthur@youritsolutions.ie>
2020-02-13 09:30:41 +0000

    add test of Producs::find static method

---
+++ tests/Unit/ProductTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Api2cart\Products;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProductTest extends TestCase
+{
+    const API2CART_DEMO_STORE_KEY = "ed58a22dfecb405a50ea3ea56979360d";
+
+    public function test_Product_find_static_method()
+    {
+        $product = Products::find(self::API2CART_DEMO_STORE_KEY, "123456");
+
+        // test should pass if no exceptions occurred
+        // regardless of the result
+        $this->assertTrue(true);
+    }
+}
