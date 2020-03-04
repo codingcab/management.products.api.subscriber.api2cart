186ced56d70a28b76eaa11fbf88751865e149b53
Artur Hanusek <arthur@youritsolutions.ie>
2020-03-04 12:38:15 +0000

    test if empty post is not allowed

---
+++ tests/Feature/ProductsRouteTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProductsRouteTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_if_empty_post_is_not_allowed()
+    {
+        $api2cart_store_key = 'abc123';
+
+        $response = $this->json('POST',"/api/api2cart/$api2cart_store_key/products/store/0",null);
+
+        $response->assertStatus(200);
+    }
+}
