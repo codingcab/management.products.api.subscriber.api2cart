7ca58c1ffc6dba0ae5dacfb7bd859f83cd0bde26
Artur Hanusek <arthur@youritsolutions.ie>
2020-03-04 12:51:31 +0000

    test if sku is required

--- tests/Feature/ProductsRouteTest.php
+++ tests/Feature/ProductsRouteTest.php
@@ -21,4 +21,17 @@ class ProductsRouteTest extends TestCase
 
         $response->assertStatus(422);
     }
+
+    public function test_if_sku_is_required()
+    {
+        $api2cart_store_key = 'abc123';
+
+        $data = [
+            "skk" => "123" //consciously using wrong working to force ProductPostRequest validation to fail
+        ];
+
+        $response = $this->json('POST',"/api/api2cart/$api2cart_store_key/products/store/0", $data);
+
+        $response->assertStatus(422);
+    }
 }
